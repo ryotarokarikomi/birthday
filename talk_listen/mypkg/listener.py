26fdeb9720d0200d25f0c61117d6f02b9029d2c1
ryotarokarikomi <ryotaro.karikomi@gmail.com>
2023-12-22 17:20:54 +0900

    talk_listenのファイルを書き換え

--- talk_listen/mypkg/listener.py
+++ talk_listen/mypkg/listener.py
@@ -3,13 +3,13 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from person.msg import Person 
 
 def cb(msg):
   global node
-  node.get_logger().info("Listen: %d" % msg.data)
+  node.get_logger().info("Listen: %d" % msg)
 
 rclpy.init()
 node = Node("listener")
-pub = node.create_subscription(Int16, "countup", cb, 10)
+pub = node.create_subscription(Int16, "person", cb, 10)
 rclpy.spin(node)
