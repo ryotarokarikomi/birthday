164a5ac1517231c76cff1b8ddd34fa957c12a84e
Ryotaro Karikomi <ryotaro.karikomi@gmail.com>
2023-12-29 17:30:58 +0900

    テストを修正

--- test/test.bash
+++ test/test.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
 
+colcon build
 source install/setup.bash
-mkdir tmp
-touch tmp/mypkg.log
 timeout 62 ros2 launch mypkg stop_watch.launch.py > tmp/mypkg.log
 
 cat tmp/mypkg.log | grep 'minute=1, second=0'
