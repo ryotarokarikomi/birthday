01ca40b521fc4bf1a5b534a47be7f97d7f3b8f8a
Ryotaro Karikomi <ryotaro.karikomi@gmail.com>
2023-12-29 17:57:01 +0900

    テストを修正

--- test/test.bash
+++ test/test.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cd ~/ros2_ws/src
+git clone https://github.com/ryotarokarikomi/time_msgs.git
+cd ~/ros2_ws
 colcon build
-source install/setup.bash
-timeout 62 ros2 launch mypkg stop_watch.launch.py > tmp/mypkg.log
+source ~/ros2_ws/install/setup.bash 
+timeout 10 ros2 launch mypkg stop_watch.launch.py > /tmp/mypkg.log
 
-cat tmp/mypkg.log | grep 'minute=1, second=0'
+cat /tmp/mypkg.log | grep 'second=9'
