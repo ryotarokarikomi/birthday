2c79a3712bd2607617a667b5470e5e6d16c8d890
Ryotaro Karikomi <ryotaro.karikomi@gmail.com>
2023-12-29 19:11:24 +0900

    テストを修正

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,7 @@ dir=~
 cd $dir/ros2_ws/src
 git clone https://github.com/ryotarokarikomi/time_msgs.git
 cd $dir/ros2_ws
-colcon build
+colcon build --symlink-install
 source $dir/ros2_ws/install/setup.bash
 timeout 10 ros2 launch mypkg stop_watch.launch.py > /tmp/mypkg.log
 
